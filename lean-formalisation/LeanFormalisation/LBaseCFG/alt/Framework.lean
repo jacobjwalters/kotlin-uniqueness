c5b291faac52d6baa8a90cf4da74fb39fb634863
Jacopo Moretti <quartztz@proton.me>
2026-04-01 15:05:23 +0200

    feat: weak soundness

--- lean-formalisation/LeanFormalisation/LBaseCFG/alt/Framework.lean
+++ lean-formalisation/LeanFormalisation/LBaseCFG/alt/Framework.lean
@@ -32,6 +32,35 @@ class Framework
     ∀ (f : fact A), IsForwardPostFixpoint (stmtCFG s) nodeTransfer edgeTransfer entryInit f →
     abs σ (f n) → abs σ' (f n')
 
+/-- for all reachable CEK states σ, there exists a state n such that -/
+theorem dataflow_soundness_exists {s : Lang .Stmt}
+    {A : Type} [Bot A] [Max A] [DecidableEq A] [FiniteHeight A]
+    {edgeTransfer : CFGEdge -> A -> A} {nodeTransfer : CFGNode -> A -> A}
+    {abs : CEK -> A -> Prop} {R : StateRel} [tr : TranslationReq s R]
+    [fr : Framework A edgeTransfer nodeTransfer s R abs]
+    (entryInit : A) (out0 : fact A)
+    (wl0 : List CFGNode) (hwl0 : ∀ x ∈ wl0, x ∈ (stmtCFG s).nodes)
+    (h_inv0 : ∀ m, m ∉ wl0 →
+        nodeTransfer m (expectedIn (stmtCFG s) edgeTransfer entryInit out0 m) ⊔ out0 m = out0 m)
+    (hinit_sound : abs (initState s) entryInit)
+    (σ : CEK) (hreach : CEKReach (initState s) σ) :
+    let res := worklistForwardEdge (stmtCFG s) nodeTransfer edgeTransfer entryInit out0 wl0 hwl0
+    ∃ n, R σ n ∧ abs σ (res n) := by
+  intro res
+  have h_postfix : IsForwardPostFixpoint (stmtCFG s) nodeTransfer edgeTransfer entryInit res :=
+    worklist_sound_postfixpoint (stmtCFG s) nodeTransfer edgeTransfer entryInit out0 wl0 hwl0 h_inv0
+  induction hreach with
+  | refl =>
+    refine ⟨(stmtCFG s).entry, tr.init_related, ?_⟩
+    refine fr.abs_mono ?_ (fr.abs_preserves_init hinit_sound)
+    simpa [expectedIn] using h_postfix (stmtCFG s).entry
+  | tail σ_mid h_eval ih =>
+    obtain ⟨n, hstep, habs⟩ := ih
+    obtain ⟨n', hstep', habs'⟩ := tr.step_sound hstep h_eval
+    refine ⟨n', hstep', ?_⟩
+    apply fr.step_sound hstep hstep' h_eval habs' res h_postfix
+    grind
+
 theorem dataflow_soundness {s : Lang .Stmt}
     {A : Type} [Bot A] [Max A] [DecidableEq A] [FiniteHeight A]
     {edgeTransfer : CFGEdge -> A -> A} {nodeTransfer : CFGNode -> A -> A}
